bea7e721ecb29a216d04cc4b6b4f8da9adf2d216
eking-cyfer <edwardafatsawo20@gmail.com>
2024-12-13 19:28:22 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#storing values 
+cat1=78
+cat2=98
+cat3=90
+cat4=79
+cat5=92
+
+# Finding the total of cats
+sum = cat1+cat2+cat3+cat4+cat5
+print("the sum of all the 5 cats is: ", sum)
+
+# Finding the average of cats
+average = sum/5
+print("the average of all the cat is: ", average)
